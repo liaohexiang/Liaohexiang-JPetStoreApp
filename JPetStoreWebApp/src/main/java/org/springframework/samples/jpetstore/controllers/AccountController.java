349ca9b8ffc09b0bfea3ae54136af433be53fbf3
liaohexiang <liaohexiang@gmail.com>
2014-11-15 21:32:28 +0800

    changes

--- JPetStoreWebApp/src/main/java/org/springframework/samples/jpetstore/controllers/AccountController.java
+++ JPetStoreWebApp/src/main/java/org/springframework/samples/jpetstore/controllers/AccountController.java
@@ -4,26 +4,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.RestoreAction;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.samples.jpetstore.dao.AccountDao;
 import org.springframework.samples.jpetstore.domain.Account;
 import org.springframework.samples.jpetstore.enums.Sex;
 import org.springframework.samples.jpetstore.validators.AccountValidator;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.SimpleFormController;
 
 @Controller
 @RequestMapping("/Account")
